a87d75c070d1867c01533a91d3b7d2f770ade369
Rahnuma parween <rahnumaparween001@gmail.com>
2026-09-25 07:31:26 +0530

    reverse of numbers

---
+++ basics01/day3/reverse.cpp
@@ -0,0 +1,20 @@
+//reverse a number
+
+#include<iostream>
+using namespace std;
+
+ int n;
+int main() {
+    int n;
+    int r=0;
+    cout<<"Enter a number:"<<endl;
+    cin>>n;
+    while(n>0) {
+        r=r*10;
+        r=r+(n%10);
+        n=n/10;
+    }
+    cout<<"the reverse of the number is:"<<r;
+    return 0;
+
+}
